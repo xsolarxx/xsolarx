38046cadcb3cbcdff43688eaf6937c60c78c1344
InesMedem <inesmedem93@gmail.com>
2024-02-15 08:57:14 +0100

    arreglado el image upload

--- NODE/src/api/routes/News.routes.js
+++ NODE/src/api/routes/News.routes.js
@@ -1,9 +1,10 @@
 const { isAuthAdmin } = require("../../middleware/auth.middleware");
 const { createNews } = require("../controllers/News.controllers");
+const { upload } = require("../../middleware/files.middleware");
 
 const NewsRoutes = require("express").Router();
-
-NewsRoutes.post("/create", [isAuthAdmin], createNews);
+//! upload.single("image") y upload middleware para poder subir imagenes por media
+NewsRoutes.post("/create", [isAuthAdmin], upload.single("image"), createNews);
 // NewsRoutes.get("/getbyid/:id", getById);
 
 module.exports = NewsRoutes;
