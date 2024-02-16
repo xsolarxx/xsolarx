be61d5bd3c18e123d36bda95546a24eedecfc546
Xiao Xue <anxxue@gmail.com>
2024-02-16 19:31:22 +0100

    Rating

--- NODE/src/api/models/Rating.model.js
+++ NODE/src/api/models/Rating.model.js
@@ -1,27 +1,30 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+//-------------------------------------------------------------------------------
+
 const RatingSchema = new Schema(
   {
+    // El rating en tipo estrella que el user le da a la compañía
     punctuation: {
       type: Number,
       required: true,
       min: 1,
       max: 5,
-    }, // el rating en tipo estrella para la compañía
+    }, // La puntuación que da el user a la compañía
     userPunctuation: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
     },
-    // la puntuación que da el User a la compañía
-    companyPunctuated: 
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Company",
-      }, // hace referencia a la compañía que ha sido puntuada por el User
+    // La compañía que ha sido puntuada por el user
+    companyPunctuated: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Company",
+    },
   },
   { timestamps: true }
 );
 
-// Create Company model
 const Rating = mongoose.model("Rating", RatingSchema);
 module.exports = Rating;
+
+// Adición de comentarios junto a
