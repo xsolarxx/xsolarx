dd927daa92750cbad44cd0973c66020e373c62d9
Xiao Xue <anxxue@gmail.com>
2024-02-18 15:06:52 +0100

    Rating controller update option

--- NODE/src/api/routes/Rating.routes.js
+++ NODE/src/api/routes/Rating.routes.js
@@ -1,10 +1,19 @@
-const express = require("express");
 const { isAuth } = require("../../middleware/auth.middleware");
-const { createRating } = require("../controllers/Rating.controllers");
-const { getAll, getById } = require("../controllers/Rating.controllers");
-const RatingRoutes = express.Router();
+
+const {
+  createRating,
+  getAll,
+  getById,
+  update,
+} = require("../controllers/Rating.controllers");
+
+const RatingRoutes = require("express").Router();
 
 RatingRoutes.post("/create", [isAuth], createRating);
 RatingRoutes.get("/getall", getAll);
 RatingRoutes.get("/getbyid/:id", getById);
+RatingRoutes.patch("/update/:id", [isAuth], update);
+
 module.exports = RatingRoutes;
+
+//Ok
