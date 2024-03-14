cba3acd650e6c8aabb353286f8a35ae18a5a195b
Xiao Xue <anxxue@gmail.com>
2024-03-14 13:28:20 +0100

    Changing rating location

--- NODE/src/api/controllers/Company.controllers.js
+++ NODE/src/api/controllers/Company.controllers.js
@@ -105,7 +105,9 @@ const getAll = async (req, res, next) => {
 const getById = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const companyById = await Company.findById(id);
+    const companyById = await Company.findById(id).populate(
+      "userCompanyRatings"
+    );
     if (companyById) {
       return res.status(200).json(companyById);
     } else {
